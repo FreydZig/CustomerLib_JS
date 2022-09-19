a3246ba9f7bf403d6ffd30b45cd110c119c2fec7
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-19 16:13:11 +0700

    add: Entities

---
+++ src/Customer.js
@@ -0,0 +1,25 @@
+const { Person } = require("Person");
+
+class Customer extends Person {
+  constructor(
+    firstName,
+    lastName,
+    addresses,
+    phoneNumber,
+    email,
+    notes,
+    totalPurchasesAmount,
+    lastPurchaseDate
+  ) {
+    super(firstName, lastName);
+
+    this.Address = addresses;
+    this.PhoneNumber = phoneNumber;
+    this.Email = email;
+    this.Notes = notes;
+    this.TotalPurchasesAmount = totalPurchasesAmount;
+    this.LastPurchaseDate = lastPurchaseDate;
+  }
+}
+
+module.exports = { Customer };
---
+++ src/Person.js
@@ -0,0 +1,8 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.FirstName = firstName;
+    this.LastName = lastName;
+  }
+}
+
+module.exports = { Person };
